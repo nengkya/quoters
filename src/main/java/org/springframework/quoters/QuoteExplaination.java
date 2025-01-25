082285dc1824f331a4f1f2b468fd49c3ccddef8e
nengkya <nengkya@yahoo.com>
2025-01-25 21:24:41 +0700

    repository pattern

---
+++ src/main/java/org/springframework/quoters/QuoteExplaination.java
@@ -0,0 +1,16 @@
+/*
+	1. create spring initializer
+
+	https://java-design-patterns.com/patterns/repository/#programmatic-example-of-repository-pattern-in-java
+	2.a. create QuoteController.java that create Quote object
+
+	  b. automatic create QuoteRepository
+
+	  c. List<QuoteResource>
+
+	3. create Quote.java object class
+
+	
+
+
+*/
